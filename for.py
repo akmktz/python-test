0dd8962d508acb886195bd8af064c306ea9f64a9
AKh <konopkin.a.wezom@gmail.com>
2019-02-13 23:19:19 +0200

    If, For

---
+++ for.py
@@ -0,0 +1,18 @@
+### While
+print('while ---------------------------')
+i = 5
+while i > 0:
+    print(i)
+    i -= 1
+
+### For
+print('for ---------------------------')
+
+for i in range(1, 10):
+    print(i)
+
+list = ['aaaa', 'bbb', 'cc', 'd']
+for item in list:
+    print(item)
+for i in range(len(list)):
+    print(list[i])
---
+++ if.py
@@ -0,0 +1,13 @@
+print('Hello world!')
+
+### If
+n = int(input('Введи число? '))
+if n < 0:
+	print('Меньше нуля')
+elif n > 0:
+	print('Больше нуля')
+else:
+	print('Ноль')
+
+
+
